4478b3ed7a1f9e4ceaedda886269ce1d8ec691e3
Pitchfork :3 <kanonenwagen7@gmail.com>
2026-05-07 10:26:45 -0400

    added dash

--- sprite.c
+++ sprite.c
@@ -30,23 +30,26 @@ unsigned char y_pos = 120;
 unsigned char speed = 1;
 unsigned char playervis = 1;
 unsigned char hasdash = 1;
-unsigned char dashspeed = 50;
+unsigned char dashspeed = 30;
+unsigned char dashtimelength = 2;
+unsigned char dashtimer = 2 * 60;
+
 // 1: down, 2: Up, 3: left, 4: right
 unsigned char lastdir = 0;
 
 /*{pal:"nes",layout:"nes"}*/
 const char PALETTE[32] = { 
-  0x03,			// screen color
+  0x2E,			// screen color
 
-  0x11,0x30,0x27,0x0,	// background palette 0
-  0x1c,0x20,0x2c,0x0,	// background palette 1
-  0x00,0x10,0x20,0x0,	// background palette 2
-  0x06,0x16,0x26,0x0,   // background palette 3
+  0x07,0x00,0x27,0x00,	// background palette 0
+  0x1C,0x20,0x2C,0x00,	// background palette 1
+  0x00,0x10,0x20,0x00,	// background palette 2
+  0x06,0x16,0x26,0x00,   // background palette 3
 
-  0x16,0x35,0x24,0x0,	// sprite palette 0
-  0x00,0x37,0x25,0x0,	// sprite palette 1
-  0x0d,0x2d,0x3a,0x0,	// sprite palette 2
-  0x0d,0x27,0x2a	// sprite palette 3
+  0x00,0x16,0x00,0x00,	// sprite palette 0
+  0x00,0x37,0x25,0x00,	// sprite palette 1
+  0x0D,0x2D,0x3A,0x00,	// sprite palette 2
+  0x0D,0x27,0x2A	// sprite palette 3
 };
 
 // setup PPU and tables
@@ -70,6 +73,13 @@ void main(void)
     ppu_wait_nmi();
     pad = pad_poll(0);
     oam_clear();
+    if (hasdash == 0) {
+      dashtimer -= 1;
+      if (dashtimer < 1) {
+        dashtimer = dashtimelength * 60;
+        hasdash = 1;
+      }
+    }
     if (playervis == 1) {
     oam_spr(x_pos, y_pos, PLAYER_TILE, PLAYER_PALETTE, 0);
     
